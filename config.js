8682ce326bf5e2b2d7803a27495d943a88bdb488
cwen0224 <cwen0224@gmail.com>
2026-03-06 17:58:06 +0800

    Configure Google sign-in client

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 window.EGDA_CONFIG = {
   appVersion: "v2026.03.06",
-  submissionEndpoint: "https://script.google.com/macros/s/AKfycbxMDOdNYYT-o2Nf3n-3XHejMGBbHVK08iQpE07OKBlyznYuOBoQSM4mQym3H0Tdqy1D/exec",
+  submissionEndpoint: "https://script.google.com/macros/s/AKfycbzJ_oQTU4F0WCY8zVnDjp4lPeH2lvW8sMUVCp7sjie85fuTrtt-c3JEewEE6k9fSJos/exec",
   requestMethod: "POST",
   draftStorageKey: "egda-registration-draft",
   maxFileSizeMb: 100,
   locale: "auto",
-  googleClientId: "",
+  googleClientId: "114769317222-agrcn2apovvh3ubbu6fpelcq6imbv1hv.apps.googleusercontent.com",
   asanaAssigneeOptions: [
     { label: "不指定", gid: "" },
     { label: "cwen0224@gmail.com", gid: "1203170629963294" }
